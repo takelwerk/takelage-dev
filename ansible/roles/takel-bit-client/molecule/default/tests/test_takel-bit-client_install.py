fc9d4b141cf183e74e49c600deab106b7042d1bf
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-07 11:46:43 +0200

    Rename unit test file

--- ansible/roles/takel-bit-client/molecule/default/tests/test_takel-bit-client_install.py
+++ ansible/roles/takel-bit-client/molecule/default/tests/test_takel-bit-client_install.py
@@ -4,10 +4,10 @@ testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_bit_client_install_packages_installed(host, testvars):
-    takel_gpg_install_packages = \
+    takel_bit_client_install_packages = \
         testvars['takel_bit_client_deb_install_packages']
 
-    for package in takel_gpg_install_packages:
+    for package in takel_bit_client_install_packages:
         deb = host.package(package)
 
         assert deb.is_installed
