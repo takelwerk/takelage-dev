5b9a134b0471eb6b546ed7470576c9a6e1a9614f
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-08 12:27:47 +0200

    Fix takelage system tests

--- ansible/roles/takel-takelage/molecule/default/system/test_takel_takelage_system_entrypoint.py
+++ ansible/roles/takel-takelage/molecule/default/system/test_takel_takelage_system_entrypoint.py
@@ -2,19 +2,26 @@ import takeltest
 
 testinfra_hosts = takeltest.hosts()
 
-def test_takel_takelage_system_entrypoint(host):
 
+@pytest.mark.parametrize("userid", [500, 99, 1000],
+                         ids=lambda uid: 'uid: ' + str(uid))
+@pytest.mark.parametrize("groupid", [20, 21, 1000],
+                         ids=lambda gid: 'gid: ' + str(gid))
+def test_takel_takelage_system_entrypoint(
+        host,
+        userid,
+        groupid):
     host.run('userdel testuser')
     host.run('rm -rf /testhome')
 
-    command = '/entrypoint.py ' \
-              '--username testuser ' \
-              '--gid 1500 ' \
-              '--uid 1600 ' \
-              '--home /testhome/testuser ' \
-              '--no-gpg ' \
-              '--no-ssh ' \
-              '--no-git ' \
+    command = '/entrypoint.py ' + \
+              '--username testuser ' + \
+              '--uid ' + str(userid) + ' ' + \
+              '--gid ' + str(groupid) + ' ' + \
+              '--home /testhome/testuser ' + \
+              '--no-gpg ' + \
+              '--no-ssh ' + \
+              '--no-git ' + \
               '--no-docker'
 
     assert host.run_test(command)
@@ -24,13 +31,13 @@ def test_takel_takelage_system_entrypoint(host):
     assert home.is_directory
     assert home.user == 'testuser'
     assert home.group == 'testuser'
-    assert home.uid == 1500
-    assert home.gid == 1600
+    assert home.uid == userid
+    assert home.gid == groupid
     assert home.mode == 0o755
 
     user = host.user('testuser')
-    assert user.uid == 1500
-    assert user.gid == 1600
+    assert user.uid == userid
+    assert user.gid == groupid
     assert user.home == '/testhome/testuser'
     assert user.shell == '/bin/bash'
     assert 'sudo' in user.groups
