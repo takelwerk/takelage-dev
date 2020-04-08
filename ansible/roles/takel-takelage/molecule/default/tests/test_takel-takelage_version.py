33a0d67315531247a0ba86b284a705be70f7a316
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-08 22:04:07 +0000

    Migrate to molecule 3

---
+++ ansible/roles/takel-takelage/molecule/default/tests/test_takel-takelage_version.py
@@ -0,0 +1,16 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_takelage_version_file(host, testvars):
+    version = testvars['project']['version']
+
+    file = host.file('/etc/takelage_version')
+
+    assert file.exists
+    assert file.is_file
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o644
+    assert file.content_string == version
