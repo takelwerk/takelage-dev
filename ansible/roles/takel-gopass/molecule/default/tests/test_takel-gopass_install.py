1c063658a02d8beae0bd6b3443b611d0564880cb
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-10 20:05:26 +0000

    Add pass symlink

--- ansible/roles/takel-gopass/molecule/default/tests/test_takel-gopass_install.py
+++ ansible/roles/takel-gopass/molecule/default/tests/test_takel-gopass_install.py
@@ -16,3 +16,9 @@ def test_takel_gopass_install_deb_package_removed(host, testvars):
     deb = testvars['takel_gopass_tmp']
 
     assert not host.file(deb).exists
+
+
+def test_takel_anarchism_install_symlink_pass(host):
+    gopass = '/usr/local/bin/gopass'
+    symlink_pass = host.file('/usr/local/bin/pass')
+    assert symlink_pass.linked_to == gopass
