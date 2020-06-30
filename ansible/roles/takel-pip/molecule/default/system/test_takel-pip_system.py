350db0e173a46a6e65a9f3b150a493b32da4da60
Stephan Wehrheim <swehrheim@geospin.de>
2020-06-30 17:39:11 +0200

    Update takel-pip

--- ansible/roles/takel-pip/molecule/default/system/test_takel-pip_system.py
+++ ansible/roles/takel-pip/molecule/default/system/test_takel-pip_system.py
@@ -1,60 +1,16 @@
-from packaging import version
-import pytest
-import re
 import takeltest
 
 testinfra_hosts = takeltest.hosts()
 
 
-@pytest.fixture(scope='module', name='pytest_file')
-def put_pytest_file(host, moleculebook):
-    pytest_file_path = '/tmp/test_pytest.py'
-    pytest_file_content = """\
-import pytest
-
-def test_pytest():
-    assert True
-"""
-
-    playbook = moleculebook.get()
-    args = dict(content=pytest_file_content,
-                dest=pytest_file_path,
-                mode='0755')
-    task = dict(action=dict(module='copy',
-                            args=args))
-    playbook['tasks'].append(task)
-    moleculebook.set(playbook)
-    moleculebook.run()
-
-    pytest_file = host.file(pytest_file_path)
-
-    assert pytest_file.exists
-    assert pytest_file.is_file
-    assert pytest_file.mode == 0o755
-    assert pytest_file.content_string == pytest_file_content
-
-
-@pytest.fixture(scope='module', name='pytest_output')
-def get_pytest_output(host, pytest_file):
-    return host.check_output('pytest /tmp/test_pytest.py')
-
-
-def test_takel_pip_system_pytest_available(pytest_output):
-    assert 'pytest' in pytest_output
-
-
-def test_takel_pip_system_pytest_version(pytest_output, testvars):
-    pip_packages = testvars['takel_pip_packages']
-    pytest_package = \
-        [package for package in pip_packages if package['name'] == 'pytest']
-    pytest_version_expected = version.parse(pytest_package[0]['version'])
-    pytest_version_search = re.search(
-        r'.*pytest-(\d{1,2}\.\d{1,2}\.?\d{1,2}?).*',
-        pytest_output,
-        re.MULTILINE)
-
-    if pytest_version_search is not None:
-        pytest_version = version.parse(pytest_version_search.group(1))
-        assert pytest_version == pytest_version_expected
-    else:
-        assert False, 'Unable to get pytest version'
+def test_takel_pip_system_check_version(host, testvars):
+    expected_pip_packages = testvars['takel_pip_packages']
+    for package in expected_pip_packages:
+        assert host.run_test(f"python3 -c 'import {package['name']}'"), \
+            f"Failed to import {package['name']}"
+        installed_version = host.check_output(
+            f"python3 -c 'import {package['name']}; "
+            f"print({package['name']}.__version__)'")
+        assert package['version'] == installed_version, \
+            (f"Expected version for {package['name']} is {installed_version}, "
+            f"but {package['version']}")
--- ansible/roles/takel-pip/molecule/default/tests/test_takel-pip_install.py
+++ ansible/roles/takel-pip/molecule/default/tests/test_takel-pip_install.py
@@ -14,22 +14,18 @@ def test_takel_pip_install_deb_packages_installed(host, testvars):
         assert package.is_installed
 
 
-@pytest.fixture(scope='module', name='packages')
+@pytest.fixture(scope='module', name='installed_pip_packages')
 def get_packages(host, testvars):
     return host.check_output('pip3 list')
 
-
-def test_takel_pip_install_pip_packages_installed(packages, testvars):
-    for package in testvars['takel_pip_packages']:
-        if '[' not in package['name']:
-            assert package['name'] in packages
-
-
-def test_takel_pip_install_pip_packages_version(packages, testvars):
-    for package in testvars['takel_pip_packages']:
-        if '[' not in package['name']:
-            search = re.search(package['name'] +
-                               '.*?' +
-                               package['version'],
-                               packages)
-            assert search is not None
+def test_takel_pip_check_version(installed_pip_packages,
+                                        host,
+                                        testvars):
+    expected_pip_packages = testvars['takel_pip_packages']
+    for package in expected_pip_packages:
+        installed = re.search(package['name'] + r'\s+(.*)',
+                              installed_pip_packages)
+        if installed is not None:
+            assert package['version'] == installed.group(1).strip()
+        else:
+            assert False, f"{package['name']} is not installed."
