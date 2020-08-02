f5ae9ccfe3c4a2e059462fd0e65ba2b0fa79d150
Stephan <swehrheim@geospin.de>
2020-08-02 03:57:11 +0200

    Test version file

--- ansible/roles/takel-takelage/molecule/default/tests/test_takel-takelage_version.py
+++ ansible/roles/takel-takelage/molecule/default/tests/test_takel-takelage_version.py
@@ -6,7 +6,6 @@ testinfra_hosts = takeltest.hosts()
 def test_takel_takelage_version_file(host, testvars):
     if 'takel_takelage_version' in testvars:
         version = testvars['takel_takelage_version']
-
         file = host.file('/etc/takelage_version')
 
         assert file.exists
