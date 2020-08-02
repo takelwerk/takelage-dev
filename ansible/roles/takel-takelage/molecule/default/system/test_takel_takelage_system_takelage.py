aa50ba1da0ec733a6985e0c05509bdb05c53df32
Stephan <swehrheim@geospin.de>
2020-08-03 00:19:53 +0200

    Fix tests

--- ansible/roles/takel-takelage/molecule/default/system/test_takel_takelage_system_takelage.py
+++ ansible/roles/takel-takelage/molecule/default/system/test_takel_takelage_system_takelage.py
@@ -4,28 +4,22 @@ testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_takelage_system_takelage(host):
-    command = 'cat /etc/debian_version'
-    debian_version = host.run(command).stdout.strip("\n")
+    command = 'cat /etc/takelage_version'
+    takelage_version = host.run(command).stdout.strip("\n")
 
-    command = 'cp /etc/debian_version /etc/takelage_version'
-    host.run(command)
-
-    command = '/usr/local/bin/takelage --summary'
+    command = '/usr/local/bin/takelage'
     output = host.run(command)
-    expected = 'takelage: \x1b[32m' + debian_version + '\x1b[00m'
+    expected = 'takelage: \x1b[32m' + takelage_version + '\x1b[00m'
 
     assert expected in output.stdout
 
 
 def test_takel_takelage_system_takelage_summary(host):
-    command = 'cat /etc/debian_version'
-    debian_version = host.run(command).stdout.strip("\n")
-
-    command = 'cp /etc/debian_version /etc/takelage_version'
-    host.run(command)
+    command = 'cat /etc/takelage_version'
+    takelage_version = host.run(command).stdout.strip("\n")
 
     command = '/usr/local/bin/takelage --summary'
     output = host.run(command)
-    expected = 'takelage: \x1b[32m' + debian_version + '\x1b[00m'
+    expected = 'takelage: \x1b[32m' + takelage_version + '\x1b[00m'
 
     assert expected in output.stdout
