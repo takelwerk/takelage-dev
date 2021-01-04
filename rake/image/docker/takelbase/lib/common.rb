8ad381d17ebc03ddf5d858f9eb652657772904b3
Stephan <swehrheim@geospin.de>
2021-01-04 15:09:33 +0100

    tau pull

--- rake/image/docker/takelbase/lib/common.rb
+++ rake/image/docker/takelbase/lib/common.rb
@@ -39,5 +39,14 @@ require 'rake'
              "--var 'target_tag=%<project_environment>s' " \
              'templates/docker/takelbase/project/build_from_prod.json',
   test: 'TAKELAGE_PROJECT_ENV=%<project_environment>s bash -c ' \
-        "'cd ansible && molecule test --scenario-name image'"
+        "'cd ansible && molecule test --scenario-name image'",
+  login: 'TAKELAGE_PROJECT_ENV=%<project_environment>s bash -c ' \
+        "'cd ansible && molecule login --scenario-name image'",
+  create: 'TAKELAGE_PROJECT_ENV=%<project_environment>s bash -c ' \
+        "'cd ansible && molecule create --scenario-name image'",
+  verify: 'TAKELAGE_PROJECT_ENV=%<project_environment>s bash -c ' \
+        "'cd ansible && molecule verify --scenario-name image'",
+  destroy: 'TAKELAGE_PROJECT_ENV=%<project_environment>s bash -c ' \
+        "'cd ansible && molecule destroy --scenario-name image'"
+
 }
