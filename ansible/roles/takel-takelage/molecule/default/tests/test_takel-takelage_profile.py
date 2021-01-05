fdf4d84088b2f4cff34257ee992519d2758b7ef3
Stephan Wehrheim <swehrheim@geospin.de>
2021-01-05 16:32:41 +0100

    Fix linting

--- ansible/roles/takel-takelage/molecule/default/tests/test_takel-takelage_profile.py
+++ ansible/roles/takel-takelage/molecule/default/tests/test_takel-takelage_profile.py
@@ -7,8 +7,8 @@ def test_takel_takelage_profile_files(host, testvars):
     profile_files = testvars['takel_takelage_profile']
     for profile_file in profile_files:
         file = host.file('/etc/profile.d/' +
-                         str(profile_file['order']) +
-                         profile_file['file'])
+            str(profile_file['order']) +
+            profile_file['file'])
 
         assert file.exists
         assert file.is_file
