db11e9c5a7cbb933f999b56c301f029a4bb952c9
Cucumber <cucumber@example.com>
2021-02-10 00:16:18 +0100

    Fix takel-rvm config

--- ansible/roles/takel-rvm/molecule/default/system/test_takel-rvm_system.py
+++ ansible/roles/takel-rvm/molecule/default/system/test_takel-rvm_system.py
@@ -10,3 +10,11 @@ def test_takel_rvm_system_rvm_available(host, testvars):
         rvm_version_output = host.check_output(f"{rvm_binary_path} version")
 
     assert 'https://rvm.io' in rvm_version_output
+
+
+def test_takel_rvm_system_ruby_available(host, testvars):
+    rvm_user = testvars['takel_rvm_user']
+    with host.sudo(rvm_user):
+        rvm_version_output = host.check_output("ruby --version")
+
+    assert '[x86_64-linux]' in rvm_version_output
