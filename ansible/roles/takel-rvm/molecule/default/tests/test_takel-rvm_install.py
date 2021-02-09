c5a4ea13858f61dade9532fe4e49f33edc0c80f3
Stephan Wehrheim <swehrheim@geospin.de>
2021-02-09 17:41:14 +0100

    Add unit tests

--- ansible/roles/takel-rvm/molecule/default/tests/test_takel-rvm_install.py
+++ ansible/roles/takel-rvm/molecule/default/tests/test_takel-rvm_install.py
@@ -10,3 +10,16 @@ def test_takel_rvm_install_deb_packages_installed(host, testvars):
         package = host.package(install_package)
 
         assert package.is_installed
+
+
+def test_takel_rvm_install_binary_available(host, testvars):
+    rvm_user = testvars['takel_rvm_user']
+    rvm_binary_path = testvars['takel_rvm_rvm_binary']
+    rvm_binary = host.file(rvm_binary_path)
+
+    assert rvm_binary.exists
+    assert rvm_binary.is_file
+    assert rvm_binary.user == 'rvm_user'
+    assert rvm_binary.group == 'rvm_user'
+    assert rvm_binary.mode == 0o755
+    assert rvm_binary.exists
