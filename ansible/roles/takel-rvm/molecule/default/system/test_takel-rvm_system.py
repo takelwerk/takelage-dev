27123faf5ae7a143e0415c703b75bc88c077b304
Stephan Wehrheim <swehrheim@geospin.de>
2021-02-10 14:34:02 +0100

    Refactor tests and ensure idempotence

--- ansible/roles/takel-rvm/molecule/default/system/test_takel-rvm_system.py
+++ ansible/roles/takel-rvm/molecule/default/system/test_takel-rvm_system.py
@@ -14,7 +14,9 @@ def test_takel_rvm_system_rvm_available(host, testvars):
 
 def test_takel_rvm_system_ruby_available(host, testvars):
     rvm_user = testvars['takel_rvm_user']
+    rvm_install_path = testvars['takel_rvm_install_path']
+    ruby_binary_path = f"{rvm_install_path}/wrappers/default/ruby"
     with host.sudo(rvm_user):
-        rvm_version_output = host.check_output("ruby --version")
+        rvm_version_output = host.check_output(f"{ruby_binary_path} --version")
 
     assert '[x86_64-linux]' in rvm_version_output
