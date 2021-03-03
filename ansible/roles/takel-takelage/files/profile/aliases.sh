db9119a075179116f77e63fe53d2c12c81edea8d
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-04 00:13:22 +0100

    Add glog alias

--- ansible/roles/takel-takelage/files/profile/aliases.sh
+++ ansible/roles/takel-takelage/files/profile/aliases.sh
@@ -1,4 +1,5 @@
-alias l='ls -laF'
+alias l='ls -lahF'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias pwpass='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang de'
 alias pwpassde='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang de'
 alias pwpassen='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang en'
