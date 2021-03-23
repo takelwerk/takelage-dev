1de15daf0c77aea3545d84fc0b4c57c92a834915
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-23 18:21:41 +0100

    Add test for mutagen-agents.tar.gz

--- ansible/roles/takel-mutagen/molecule/default/tests/test_takel-mutagen_install.py
+++ ansible/roles/takel-mutagen/molecule/default/tests/test_takel-mutagen_install.py
@@ -22,3 +22,14 @@ def test_takel_mutagen_install_mutagen_installed(host, testvars):
     assert file.user == 'root'
     assert file.group == 'root'
     assert file.mode == 0o755
+
+
+def test_takel_mutagen_install_mutagen_agents_installed(host, testvars):
+    mutagen_bin = f"{testvars['takel_mutagen_bin_path']}/mutagen-agents.tar.gz"
+    file = host.file(mutagen_bin)
+
+    assert file.exists
+    assert file.is_file
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o644
