7b0ed23b96ddbfe0a6823cb97cf3ecd0473bd4a0
Stephan Wehrheim <swehrheim@geospin.de>
2021-03-26 00:58:03 +0100

    Colorize ls

--- ansible/roles/takel-takelage/files/profile/aliases.sh
+++ ansible/roles/takel-takelage/files/profile/aliases.sh
@@ -1,3 +1,4 @@
+alias ls='ls --color=auto'
 alias l='ls -lahF'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias pwpass='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang de'
