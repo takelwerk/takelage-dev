3b88ef1bf4a56a520c7a2d8d2bb021eda1dd9a4c
Stephan Wehrheim <swehrheim@geospin.de>
2021-06-10 01:53:00 +0200

    Fix image test

--- ansible/molecule/image/tests/test_image_meta.py
+++ ansible/molecule/image/tests/test_image_meta.py
@@ -15,7 +15,7 @@ def test_image_meta_cmd(testvars, image_meta_data):
     if 'command' in testvars['project']['images'][image]:
         expected = testvars['project']['images'][image]['command']
     else:
-        expected = '/usr/bin/tail -f /dev/null'
+        expected = '/bin/sh -c "/usr/bin/tail -f /dev/null"'
     assert expected.split(' ') == image_meta_data['Config']['Cmd']
 
 
