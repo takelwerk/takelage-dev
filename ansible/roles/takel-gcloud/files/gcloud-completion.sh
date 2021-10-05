c00da7a9970943d3cf44bd59890875a29829803b
Stephan <stephan@takelwerk.net>
2021-10-05 23:11:34 +0200

    Clean up repo

--- ansible/roles/takel-gcloud/files/gcloud-completion.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/opt/google-cloud-sdk/path.bash.inc' ]; then . '/opt/google-cloud-sdk/path.bash.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/opt/google-cloud-sdk/completion.bash.inc' ]; then . '/opt/google-cloud-sdk/completion.bash.inc'; fi
