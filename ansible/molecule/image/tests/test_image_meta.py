ed1c3d854df4120956dcc4773bf4d897d211846a
Stephan <stephan@takelwerk.net>
2021-10-09 02:48:48 +0200

    Fix molecule container name

--- ansible/molecule/image/tests/test_image_meta.py
+++ ansible/molecule/image/tests/test_image_meta.py
@@ -50,7 +50,7 @@ def test_container_process(host, process, expected_args):
 def test_container_process_tail(host):
     successfully_login = False
     command = '/usr/bin/docker exec ' \
-              '--tty molecule_takelage_dev_test_image ' \
+              '--tty molecule-takelage-dev-test-image ' \
               '/loginpoint.py --username testuser'.split(' ')
     try:
         subprocess.run(command,
