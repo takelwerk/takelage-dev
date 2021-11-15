d3f61dd9ca1b8da0fc9c88e676becae0c281d4f1
Stephan <stephan@takelwerk.net>
2021-11-15 13:31:46 +0100

    Add bat alias

--- ansible/roles/takel_takelage/files/profile/aliases.sh
+++ ansible/roles/takel_takelage/files/profile/aliases.sh
@@ -1,6 +1,7 @@
+alias bat='batcat'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias ls='ls --color=auto'
 alias l='ls -lahF'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias pwpass='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang de'
 alias pwpassde='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang de'
 alias pwpassen='gopass pwgen --one-per-line --xkcd --xkcdsep - --xkcdlang en'
