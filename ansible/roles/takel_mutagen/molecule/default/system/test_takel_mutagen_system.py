714bf916712e389992ff50afda1ddc5028d6809c
Stephan <stephan@takelwerk.net>
2023-01-08 17:58:57 +0100

    Satisfy linter

--- ansible/roles/takel_mutagen/molecule/default/system/test_takel_mutagen_system.py
+++ ansible/roles/takel_mutagen/molecule/default/system/test_takel_mutagen_system.py
@@ -6,7 +6,8 @@ testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_mutagen_system_mutagen_version(host, testvars):
-    takel_mutagen_mutagen_version = str(testvars['takel_mutagen_mutagen_version'])
+    takel_mutagen_mutagen_version = \
+        str(testvars['takel_mutagen_mutagen_version'])
     mutagen_version_output = \
         host.check_output('mutagen version')
     mutagen_version_search = re.search(
@@ -15,7 +16,8 @@ def test_takel_mutagen_system_mutagen_version(host, testvars):
     assert mutagen_version_search is not None, 'Unable to get mutagen version'
 
     if takel_mutagen_mutagen_version == 'latest':
-        response = requests.get("https://api.github.com/repos/mutagen-io/mutagen/releases/latest")
+        url = "https://api.github.com/repos/mutagen-io/mutagen/releases/latest"
+        response = requests.get(url)
         mutagen_version = response.json()["tag_name"]
     assert f"v{mutagen_version_search.group(1)}" == mutagen_version, \
         'Unable to get mutagen version'
