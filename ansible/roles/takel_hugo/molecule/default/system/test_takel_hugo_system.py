aec71bc05dc15e6c01800d8bc1e4d5a8dca79f33
Stephan <stephan@takelwerk.net>
2023-01-08 17:02:49 +0100

    Fix latest version test

--- ansible/roles/takel_hugo/molecule/default/system/test_takel_hugo_system.py
+++ ansible/roles/takel_hugo/molecule/default/system/test_takel_hugo_system.py
@@ -1,14 +1,17 @@
 import takeltest
 import re
+import requests
 
 testinfra_hosts = takeltest.hosts()
 
 
-def test_takel_hugo_system_hugo_version(host, testvars):
-    hugo_version = str(testvars['takel_hugo_hugo_version'])
+def test_takel_hugo_system_hugo_version(host):
+    response = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest")
+    hugo_version = response.json()["tag_name"]
+
     hugo_version_output = host.check_output('hugo version')
     hugo_version_search = re.search(
-        r'.*(\d{1,2}\.\d{1,3}\.\d{0,2}).*', hugo_version_output)
+        r'.*(v\d{1,2}\.\d{1,3}\.\d{0,2}).*', hugo_version_output)
 
     if hugo_version_search is not None:
         assert hugo_version_search.group(1) == hugo_version
