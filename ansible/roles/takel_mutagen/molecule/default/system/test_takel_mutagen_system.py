98bcc7ea49415b823a52494023082b1458f98df8
Stephan <stephan@takelwerk.net>
2023-01-08 17:57:38 +0100

    Fix mutagen install latest version

--- ansible/roles/takel_mutagen/molecule/default/system/test_takel_mutagen_system.py
+++ ansible/roles/takel_mutagen/molecule/default/system/test_takel_mutagen_system.py
@@ -1,12 +1,12 @@
 import takeltest
 import re
-import subprocess
+import requests
 
 testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_mutagen_system_mutagen_version(host, testvars):
-    takel_mutagen_version = str(testvars['takel_mutagen_version'])
+    takel_mutagen_mutagen_version = str(testvars['takel_mutagen_mutagen_version'])
     mutagen_version_output = \
         host.check_output('mutagen version')
     mutagen_version_search = re.search(
@@ -14,17 +14,8 @@ def test_takel_mutagen_system_mutagen_version(host, testvars):
 
     assert mutagen_version_search is not None, 'Unable to get mutagen version'
 
-    if takel_mutagen_version == 'latest':
-        url = "https://api.github.com/repos/mutagen-io/mutagen/releases/latest"
-        command = \
-            '/usr/bin/curl -sL ' \
-            f"{url} | " \
-            '/usr/bin/jq -r ".tag_name"'
-        processes = subprocess.run(
-            command,
-            shell=True,
-            capture_output=True,
-            text=True)
-        takel_mutagen_version = processes.stdout.split('v')[-1].strip()
-    assert mutagen_version_search.group(1) == takel_mutagen_version, \
+    if takel_mutagen_mutagen_version == 'latest':
+        response = requests.get("https://api.github.com/repos/mutagen-io/mutagen/releases/latest")
+        mutagen_version = response.json()["tag_name"]
+    assert f"v{mutagen_version_search.group(1)}" == mutagen_version, \
         'Unable to get mutagen version'
