ee7bb77c06452f5ccb0a688af3b0f64f814354af
Stephan <stephan@takelwerk.net>
2023-01-08 17:57:26 +0100

    Fix hugo install latest version

--- ansible/roles/takel_hugo/molecule/default/system/test_takel_hugo_system.py
+++ ansible/roles/takel_hugo/molecule/default/system/test_takel_hugo_system.py
@@ -5,15 +5,17 @@ import requests
 testinfra_hosts = takeltest.hosts()
 
 
-def test_takel_hugo_system_hugo_version(host):
-    response = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest")
-    hugo_version = response.json()["tag_name"]
-
-    hugo_version_output = host.check_output('hugo version')
+def test_takel_hugo_system_hugo_version(host, testvars):
+    takel_hugo_hugo_version = str(testvars['takel_hugo_hugo_version'])
+    hugo_version_output = \
+        host.check_output('hugo version')
     hugo_version_search = re.search(
         r'.*(v\d{1,2}\.\d{1,3}\.\d{0,2}).*', hugo_version_output)
 
-    if hugo_version_search is not None:
-        assert hugo_version_search.group(1) == hugo_version
-    else:
-        assert False, 'Unable to get hugo version'
+    assert hugo_version_search is not None, 'Unable to get hugo version'
+
+    if takel_hugo_hugo_version == 'latest':
+        response = requests.get("https://api.github.com/repos/gohugoio/hugo/releases/latest")
+        hugo_version = response.json()["tag_name"]
+    assert hugo_version_search.group(1) == hugo_version, \
+        'Unable to get hugo version'
