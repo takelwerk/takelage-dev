c5e8376a96a52a307dbf66d99d7c1f546d5dbd09
Stephan <stephan@takelwerk.net>
2023-01-09 00:36:11 +0100

    Add takel_k3d

---
+++ ansible/roles/takel_k3d/molecule/default/system/test_takel_k3d_system.py
@@ -0,0 +1,22 @@
+import takeltest
+import re
+import requests
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_k3d_system_k3d_version(host, testvars):
+    takel_k3d_k3d_version = str(testvars['takel_k3d_k3d_version'])
+    k3d_version_output = \
+        host.check_output('k3d version')
+    k3d_version_search = re.search(
+        r'.*(v\d{1,2}\.\d{1,3}\.\d{0,2}).*', k3d_version_output)
+
+    assert k3d_version_search is not None, 'Unable to get k3d version'
+
+    if takel_k3d_k3d_version == 'latest':
+        url = "https://api.github.com/repos/k3d-io/k3d/releases/latest"
+        response = requests.get(url)
+        k3d_version = response.json()["tag_name"]
+    assert k3d_version_search.group(1) == k3d_version, \
+        'Unable to get k3d version'
---
+++ ansible/roles/takel_k3d/molecule/default/tests/test_takel_k3d_install.py
@@ -0,0 +1,12 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+def test_takel_k3d_install_packages_installed(host, testvars):
+    takel_gpg_install_packages = \
+        testvars['takel_k3d_deb_install_packages']
+
+    for package in takel_gpg_install_packages:
+        deb = host.package(package)
+
+        assert deb.is_installed
