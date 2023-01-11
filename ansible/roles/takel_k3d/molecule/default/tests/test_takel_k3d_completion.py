ec4ec589e8ede056db2c80cd60933397ca85dbe4
Stephan <stephan@takelwerk.net>
2023-01-11 15:08:04 +0100

    Add kubectl bash completion

--- ansible/roles/takel_k3d/molecule/default/tests/test_takel_k3d_completion.py
+++ ansible/roles/takel_k3d/molecule/default/tests/test_takel_k3d_completion.py
@@ -3,8 +3,30 @@ import takeltest
 testinfra_hosts = takeltest.hosts()
 
 
-def test_takel_k3d_bash_completion(host, testvars):
-    completion_file = testvars['takel_k3d_bash_completion']
+def test_takel_k3d_completion_dir(host, testvars):
+    completion_dir = testvars['takel_k3d_completion_dir']
+    dir = host.file(completion_dir)
+
+    assert dir.exists
+    assert dir.is_directory
+    assert dir.user == 'root'
+    assert dir.group == 'root'
+    assert dir.mode == 0o755
+
+
+def test_takel_k3d_k3d_bash_completion(host, testvars):
+    completion_file = testvars['takel_k3d_k3d_bash_completion']
+    file = host.file(completion_file)
+
+    assert file.exists
+    assert file.is_file
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o644
+
+
+def test_takel_k3d_kubectl_bash_completion(host, testvars):
+    completion_file = testvars['takel_k3d_kubectl_bash_completion']
     file = host.file(completion_file)
 
     assert file.exists
