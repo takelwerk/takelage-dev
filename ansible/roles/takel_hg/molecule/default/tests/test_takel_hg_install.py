196b85f611786545301560a5980431bd69cbdde3
Stephan <stephan@takelwerk.net>
2023-02-09 19:26:44 +0100

    Fix hg-git

--- ansible/roles/takel_hg/molecule/default/tests/test_takel_hg_install.py
+++ ansible/roles/takel_hg/molecule/default/tests/test_takel_hg_install.py
@@ -12,18 +12,8 @@ def test_takel_hg_install_deb_packages_installed(host, testvars):
         assert package.is_installed
 
 
-def test_takel_hg_install_pip_package_dulwich(host, testvars):
+def test_takel_hg_install_pip_packages(host, testvars):
+    pip_packages = testvars['takel_hg_pip_packages']
     pip3_list_output = host.check_output('pip3 list')
-
-    assert 'dulwich' in pip3_list_output
-
-
-def test_takel_hg_install_hggit_repo(host, testvars):
-    hg_extension_dir = testvars['takel_hg_extension_dir']
-    file = host.file(f"{hg_extension_dir}/hg-git/.hg")
-
-    assert file.exists
-    assert file.is_directory
-    assert file.user == 'root'
-    assert file.group == 'root'
-    assert file.mode == 0o755
+    for pip_package in pip_packages:
+        assert pip_package['name'] in pip3_list_output
