6c9bf65b80736f335aa9914f74a9730832038bc0
Stephan <stephan@takelwerk.net>
2023-02-13 01:43:01 +0100

    Add cleanup test

---
+++ ansible/roles/takel_kubernetes/molecule/default/tests/test_takel_kubernetes_cleanup.py
@@ -0,0 +1,16 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_kubernetes_helm_tmp_targz(host, testvars):
+    tmp_dir = testvars['takel_kubernetes_helm_tmp_dir']
+
+    assert host.run_expect([2], f"ls {tmp_dir}/helm*")
+
+
+def test_takel_kubernetes_helm_tmp_targz_dir(host, testvars):
+    targz_dir = testvars['takel_kubernetes_helm_tmp_targz_dir']
+    dir = host.file(targz_dir)
+
+    assert not dir.exists
